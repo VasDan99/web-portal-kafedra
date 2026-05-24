c872f2dd5fb6ddcdda868ff4cde1e1eb9d7395d1
Vasilyev Daniil <VasilevDan99@yandex.ru>
2026-05-24 22:00:41 +0300

    Fix __init__.py and configure Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,11 @@
 from flask import Flask
-from config import Config
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
 
-    @app.route('/')git add .
-git commit -m "Initial Flask app - portal работает"
-git push
+    @app.route('/')
     def index():
-        return "<h1>Портал кафедры информационных систем</h1><p>Flask работает!</p>"
+        return '<h1>Портал кафедры информационных систем</h1><p>Flask работает!</p>'
 
     return app
