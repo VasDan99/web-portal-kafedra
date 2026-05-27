d1b8bb6b6d1fae79ea1e4aad0625c44b0218af23
Vasilyev Daniil <VasilevDan99@yandex.ru>
2026-05-27 09:01:04 +0300

    Fix: make current_user available in all templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager, current_user
 from config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.login_message = 'Пожалуйста, авторизуйтесь для доступа к этой странице'
 
 
 def create_app():
@@ -10,8 +16,24 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    # Делаем current_user доступным во всех шаблонах
+    @app.context_processor
+    def inject_user():
+        return dict(current_user=current_user)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
     return app
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User
+    return User.query.get(int(user_id))
