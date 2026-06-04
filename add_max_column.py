d84d8597c0dd99800a9cd0bb51935c39a1f3bf82
Vasilyev Daniil <VasilevDan99@yandex.ru>
2026-06-04 09:13:04 +0300

    Add script to add max_url column to site_settings table

---
+++ add_max_column.py
@@ -0,0 +1,19 @@
+import sqlite3
+import os
+
+db_path = os.path.join('instance', 'portal.db')
+conn = sqlite3.connect(db_path)
+cursor = conn.cursor()
+
+try:
+    cursor.execute('ALTER TABLE site_settings ADD COLUMN max_url VARCHAR(200)')
+    conn.commit()
+    print('Поле max_url добавлено в таблицу site_settings')
+except sqlite3.OperationalError as e:
+    if 'duplicate column name' in str(e):
+        print('Поле max_url уже существует')
+    else:
+        print(f'Ошибка: {e}')
+
+conn.close()
+print('Готово!')
