f77e193126890c935ea08fd755fadbb7a777327c
Vasilyev Daniil <VasilevDan99@yandex.ru>
2026-06-04 12:02:54 +0300

    Fix database path: always use /tmp/portal.db for deployment

--- config.py
+++ config.py
@@ -3,14 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = 'your-secret-key-here'
-    # Для деплоя на Timeweb Cloud используем /tmp/portal.db
-    # Локально можно оставить instance/portal.db
-    if os.environ.get('DEPLOY_ENV') == 'production':
-        SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/portal.db'
-    else:
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance',
-                                                              'portal.db')
-
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/portal.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     BABEL_DEFAULT_LOCALE = 'ru'
