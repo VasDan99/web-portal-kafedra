e620d4e587bf5fc9d5c7409f1a2573443839bdb2
Vasilyev Daniil <VasilevDan99@yandex.ru>
2026-07-21 10:08:02 +0300

    обавлен user_loader для Flask-Login

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, current_user
 from config import Config
+from flask_mail import Mail
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Пожалуйста, авторизуйтесь для доступа к этой странице'
+mail = Mail()
 
+# ====== USER_LOADER ДЛЯ FLASK-LOGIN ======
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User
+    return User.query.get(int(user_id))
 
 def create_app():
     app = Flask(__name__)
@@ -18,6 +25,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     @app.context_processor
     def inject_user():
@@ -30,9 +38,3 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    from app.models import User
-    return User.query.get(int(user_id))
